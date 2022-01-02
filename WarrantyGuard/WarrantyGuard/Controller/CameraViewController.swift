27cdec5f8bcb36817e5660c9ef649efc98d42d16
A A <aa@Areej.local>
2022-01-02 16:14:44 +0300

    P-2022_1_2 Notification

--- WarrantyGuard/WarrantyGuard/Controller/CameraViewController.swift
+++ WarrantyGuard/WarrantyGuard/Controller/CameraViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FirebaseStorage
+import Photos
 
 
 class CameraViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -41,27 +42,27 @@ class CameraViewController : UIViewController, UIImagePickerControllerDelegate, 
   @IBAction func nextButtonPressed(_ sender: Any) {
     
     if warrantyImageView.image != nil {
-      
-      let db = Firestore.firestore()
-      var ref: DocumentReference? = nil
-      
-//      let warrantyImage = warrantyImageView.image ?? #imageLiteral(resourceName: "done")
-      
-      guard let imageData = warrantyImageView.image?.jpegData(compressionQuality: 1) else { return }
-      
-      let imageName = UUID().uuidString
-//      let imageReference = Storage.storage().reference().child(MyKeys)
-      
-      ref = db.collection("warrantys").addDocument(data: ["warrantyImage":imageData]) { (error) in
-        
-        if error != nil {
-          // Show error message
-          print("Error saving user data")
-        }
-        else {
-          print("document added")
-        }
-      }
+//
+//      let db = Firestore.firestore()
+//      var ref: DocumentReference? = nil
+//
+////      let warrantyImage = warrantyImageView.image ?? #imageLiteral(resourceName: "done")
+//
+//      guard let imageData = warrantyImageView.image?.jpegData(compressionQuality: 1) else { return }
+//
+//      let imageName = UUID().uuidString
+////      let imageReference = Storage.storage().reference().child(MyKeys)
+//
+//      ref = db.collection("warrantys").addDocument(data: ["warrantyImage":imageData]) { (error) in
+//
+//        if error != nil {
+//          // Show error message
+//          print("Error saving user data")
+//        }
+//        else {
+//          print("document added")
+//        }
+//      }
       
       performSegue(withIdentifier: "Next", sender: nil)
       
@@ -90,6 +91,28 @@ class CameraViewController : UIViewController, UIImagePickerControllerDelegate, 
     }
     
     warrantyImageView.image = image
+    
+    if let url = info[UIImagePickerController.InfoKey.imageURL] as? URL {
+      print(url)
+      uploadToCloud(fileURL: url)
+    }
+  }
+  
+  //Upload images to firebase storage
+  func uploadToCloud(fileURL : URL){
+    let storge = Storage.storage()
+    let data = Data()
+    let storgeRef = storge.reference()
+    let localFile = fileURL
+    let photoRef = storgeRef.child("UploadPhotoOne")
+    let uploadTask = photoRef.putFile(from: localFile, metadata: nil) {
+      (metadata, err) in
+      guard let metadata = metadata else {
+        print(err?.localizedDescription)
+        return
+      }
+      print("Photo Upload")
+    }
   }
   
 }
